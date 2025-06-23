0a373c5360849bc7ac351d8b5e56917e4daec803
ltrang11 <ltrang1811@gmail.com>
2025-06-24 00:14:35 +0930

    update unpaid order

--- lib/api/order.ts
+++ lib/api/order.ts
@@ -1,8 +1,15 @@
-const API_URL = "http://192.168.202.75:10000/api";
+import Constants from 'expo-constants';
+// const API_URL = "http://192.168.202.75:10000/api";
+const API_BASE_URL =
+          Constants.expoConfig?.extra?.API_BASE_URL ??
+          Constants.manifest2?.extra?.API_BASE_URL;
+
+
 
 export async function getUnpaidOrders(customerId: string) {
   try {
-    const res = await fetch(`${API_URL}/order/checkout/${customerId}`);
+    const res = await fetch(`${API_BASE_URL}/api/orders/checkout/${customerId}`);
+    // const res = await fetch(`${API_URL}/order/checkout/${customerId}`);
     if (!res.ok) throw new Error("Failed to fetch unpaid orders");
     return await res.json();
   } catch (err) {
@@ -12,7 +19,7 @@ export async function getUnpaidOrders(customerId: string) {
 }
 
 export async function payOrder(customerId: string) {
-  const res = await fetch(`${API_URL}/payment/${customerId}`, {
+  const res = await fetch(`${API_BASE_URL}/api/orders/payment/${customerId}`, {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
   });
